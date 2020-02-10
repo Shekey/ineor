0a6e5535ed6e06eb7f55341909aeebddf38792f2
Ajdin <ajdin_sah@hotmail.com>
2020-02-10 01:30:31 +0100

    some global form styling

--- src/components/form.js
+++ src/components/form.js
@@ -14,40 +14,43 @@ const Form = (props) => {
             <div className="input-wrapper">
                 <input type="text" placeholder="First Name" />
                 <input type="text" placeholder="Last Name" />
+                <p>Please enter your full name</p>
             </div>
 
             <div className="input-wrapper">
                 <input type="email" placeholder="Email" />
+                <p>Please enter a valid email</p>
                 <input type="number" placeholder="Contact Number" />
+                <p>Please enter phone number</p>
             </div>
 
             <div className="input-wrapper">
                 <select className="barber-select">
                     <option value="default">Select Barber</option>
                 </select>
+                <p>Please select a barber</p>
 
                 <select className="service-select">
                     <option value="default">Select Service</option>
                 </select>
+                <p>Please select a service</p>
             </div>
 
             <div className="input-wrapper">
-                <DatePicker selected={new Date()} onChange={handleChange()} />
+                <DatePicker selected={null} onChange={handleChange()} placeholderText="Select Date" />
+                <p>Please pick a date</p>
 
                 <select className="time-select">
                     <option value="default">Select Time</option>
                 </select>
+                <p>Please pick a time</p>
             </div>
 
             <div className="input-wrapper input-wrapper--f-w">
-                <input type="text" placeholder="Select any service." />
+                <input type="text" readOnly placeholder="Select any service." />
             </div>
 
-            <button className="barber-form__button">book appointment</button>
-
-            <div className="input-wrapper--f-w hide">
-                <input type="number" placeholder="Price" />
-            </div>
+            <button className="barber-form__button"><span className="show-on-tablet">Book</span><span>book appointment</span></button>
         </form>
     );
 }
